d81cb764925435112c7e73b788c5795bebcbd4ed
Johnson Zhong <zhsh@umich.edu>
2020-03-28 23:32:51 -0400

    Finish tutorial code (on their data)

--- scripts/mask_rcnn_tutorial.py
+++ scripts/mask_rcnn_tutorial.py
@@ -13,6 +13,7 @@ import torch.utils.data
 from sem_to_ins.dataset import CleargraspSyntheticDataset
 from sem_to_ins.detection_reference import transforms as T
 from sem_to_ins.detection_reference import utils
+from sem_to_ins.detection_reference.engine import train_one_epoch, evaluate
 
 import random
 import torch
@@ -167,20 +168,76 @@ def get_transform(train):
     return T.Compose(transforms)
 
 
-dataset = PennFudanDataset(os.path.join(cfg.DATA_DIR, 'PennFudanPed/'), get_transform(True))
-# dataset = CleargraspSyntheticDataset(os.path.join(cfg.DATA_DIR, 'synthetic-val/'))
-print(dataset[0])
+# dataset = PennFudanDataset(os.path.join(cfg.DATA_DIR, 'PennFudanPed/'), get_transform(True))
+# # dataset = CleargraspSyntheticDataset(os.path.join(cfg.DATA_DIR, 'synthetic-val/'))
+# print(dataset[0])
+#
+# model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+# data_loader = torch.utils.data.DataLoader(
+#     dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn)
+# # For Training
+# images, targets = next(iter(data_loader))
+# images = list(image for image in images)
+# targets = [{k: v for k, v in t.items()} for t in targets]
+# output = model(images, targets)  # Returns losses and detections
+# # For inference
+# model.eval()
+# x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
+# predictions = model(x)  # Returns predictions
+# print(predictions)
 
-model = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
-data_loader = torch.utils.data.DataLoader(
-    dataset, batch_size=2, shuffle=True, num_workers=4, collate_fn=utils.collate_fn)
-# For Training
-images, targets = next(iter(data_loader))
-images = list(image for image in images)
-targets = [{k: v for k, v in t.items()} for t in targets]
-output = model(images, targets)  # Returns losses and detections
-# For inference
-model.eval()
-x = [torch.rand(3, 300, 400), torch.rand(3, 500, 400)]
-predictions = model(x)  # Returns predictions
-print(predictions)
+
+def main():
+    # train on the GPU or on the CPU, if a GPU is not available
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
+
+    # our dataset has two classes only - background and person
+    num_classes = 2
+    dataset = PennFudanDataset(os.path.join(cfg.DATA_DIR, 'PennFudanPed/'), get_transform(train=True))
+    dataset_test = PennFudanDataset(os.path.join(cfg.DATA_DIR, 'PennFudanPed/'), get_transform(train=False))
+
+    # split the dataset in train and test set
+    indices = torch.randperm(len(dataset)).tolist()
+    dataset = torch.utils.data.Subset(dataset, indices[:-50])
+    dataset_test = torch.utils.data.Subset(dataset_test, indices[-50:])
+
+    # define training and validation data loaders
+    data_loader = torch.utils.data.DataLoader(
+        dataset, batch_size=2, shuffle=True, num_workers=0,
+        collate_fn=utils.collate_fn)
+
+    data_loader_test = torch.utils.data.DataLoader(
+        dataset_test, batch_size=1, shuffle=False, num_workers=0,
+        collate_fn=utils.collate_fn)
+
+    # get the model using our helper function
+    model = get_model_instance_segmentation(num_classes)
+
+    # move model to the right device
+    model.to(device)
+
+    # construct an optimizer
+    params = [p for p in model.parameters() if p.requires_grad]
+    optimizer = torch.optim.SGD(params, lr=0.005,
+                                momentum=0.9, weight_decay=0.0005)
+    # and a learning rate scheduler
+    lr_scheduler = torch.optim.lr_scheduler.StepLR(optimizer,
+                                                   step_size=3,
+                                                   gamma=0.1)
+
+    # let's train it for 10 epochs
+    num_epochs = 10
+
+    for epoch in range(num_epochs):
+        # train for one epoch, printing every 10 iterations
+        train_one_epoch(model, optimizer, data_loader, device, epoch, print_freq=10)
+        # update the learning rate
+        lr_scheduler.step()
+        # evaluate on the test dataset
+        evaluate(model, data_loader_test, device=device)
+
+    print("That's it!")
+
+
+if __name__ == "__main__":
+    main()
